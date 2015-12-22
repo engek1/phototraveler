5df27a9641dedfc79ab1f5764ad809814fbca345
webel3 <webel3@bfh.ch>
2015-12-22 13:53:18 +0100

    added last missing service implementation.

---
+++ domain/src/main/java/ch/bfh/bti7051/phototraveler/repository/ItemCollectionRepository.java
@@ -0,0 +1,7 @@
+package ch.bfh.bti7051.phototraveler.repository;
+
+/**
+ * Created by Lukas on 22.12.2015.
+ */
+public interface ItemCollectionRepository {
+}
---
+++ service/src/main/java/ch/bfh/bti7051/phototraveler/service/services/ItemCollectionService.java
@@ -0,0 +1,7 @@
+package ch.bfh.bti7051.phototraveler.service.services;
+
+/**
+ * Created by Lukas on 22.12.2015.
+ */
+public class ItemCollectionService {
+}
